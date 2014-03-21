ad514549d62addf8c461026b45d77973de565710
aurel-esiea <pinon@et.esiea.fr>
2014-03-21 22:43:04 +0100

    Finish Alarm implementation

--- esiea_bid/src/alarm_time/AlarmObserver.java
+++ esiea_bid/src/alarm_time/AlarmObserver.java
@@ -42,31 +42,27 @@ public class AlarmObserver implements Observer{
 		
 		if(obs instanceof Offer)
 		{
-			for (Offer offer : listOffer)
+			Offer offer = (Offer)obs;
+	
+			if(offer.getPrice() >= offer.getBid().getReservePrice() && !offer.getBid().isReservePriceReached())
 			{
-				if(offer == obs)
-				{		
-					if(offer.getPrice() >= offer.getBid().getReservePrice() && !offer.getBid().isReservePriceReached())
-					{
-						offer.getBid().setReservePriceReached(true);
-						for (Alarm alarm : listAlarm)
-						{
-							if(alarm.getAlarmType().equals(AlarmType.RESERVE_PRICE_REACHED) && alarm.getBid().equals(offer.getBid()))
-								System.out.println("Alarm for " + alarm.getUser().getFirstName() + ": reserve Price on product " + offer.getBid().getProduct().getDescription() + " has been reached");
-						}
-					}	
-					
-					if(offer.getPrice() > offer.getBid().getPrice() && !offer.getBid().isFirstOffer())
-					{
-						for (Alarm alarm : listAlarm)
-						{
-							if(alarm.getAlarmType().equals(AlarmType.BEST_OFFER) && alarm.getBid().equals(offer.getBid()) && !alarm.getUser().equals(offer.getBuyer()))
-								System.out.println("Alarm for " + alarm.getUser().getFirstName() + ": user " + offer.getBuyer().getFirstName() + " do better offer on product " + offer.getBid().getProduct().getDescription());
-						}
-					}
-					System.out.println("Alarm for " + offer.getBid().getSeller().getFirstName() + ": user " + offer.getBuyer().getFirstName() + " do an offer on your bid " + offer.getBid().getProduct().getDescription());
+				offer.getBid().setReservePriceReached(true);
+				for (Alarm alarm : listAlarm)
+				{
+					if(alarm.getAlarmType().equals(AlarmType.RESERVE_PRICE_REACHED) && alarm.getBid().equals(offer.getBid()))
+						System.out.println("Alarm for " + alarm.getUser().getFirstName() + ": reserve Price on product " + offer.getBid().getProduct().getDescription() + " has been reached");
 				}
 			}	
+			
+			if(offer.getPrice() > offer.getBid().getPrice() && !offer.getBid().isFirstOffer())
+			{
+				for (Alarm alarm : listAlarm)
+				{
+					if(alarm.getAlarmType().equals(AlarmType.BEST_OFFER) && alarm.getBid().equals(offer.getBid()) && !alarm.getUser().equals(offer.getBuyer()))
+						System.out.println("Alarm for " + alarm.getUser().getFirstName() + ": user " + offer.getBuyer().getFirstName() + " do better offer on product " + offer.getBid().getProduct().getDescription());
+				}
+			}
+			System.out.println("Alarm for " + offer.getBid().getSeller().getFirstName() + ": user " + offer.getBuyer().getFirstName() + " do an offer on your bid " + offer.getBid().getProduct().getDescription());
 		}
 	}
 }
